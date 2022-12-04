6bb81a7719c1779172da2020ba65c294bdd0131f
kaiuri <uriel.acioli@gmail.com>
2022-12-03 22:16:25 -0300

    chore(poor-donkey): 2022/12/03-22:16

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use advent_of_code_2022::aoc::day_1;
 
 macro_rules! read_file {
     ($input:expr) => {
-        std::fs::read_to_string($input).unwrap()
+        std::fs::read_to_string($input).unwrap_or_else(|e| panic!("error: {e} not found"))
     };
 }
 fn main() {
     // Day 1: Calorie Counting
     let input = read_file!([env!("PWD"), "inputs/day1.txt"].join("/"));
+
     println!("{:?}", day_1(input.as_str()));
 }
